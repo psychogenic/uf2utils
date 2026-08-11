f3b49ab2f8e1b750f2864dba7314db35ab511586
Pat Deegan <pat.devel@psychogenic.com>
2026-08-11 18:39:18 -0400

    Smarter fs update

--- src/uf2utils/examples/pico_fs_update.py
+++ src/uf2utils/examples/pico_fs_update.py
@@ -34,9 +34,15 @@ def get_args():
     
     parser.add_argument('--fs_root', help='directory to use as root of filesystem', required=True)
     parser.add_argument('--out', help='output UF2 file with fs to create', required=True)
-    parser.add_argument('--fs_offset', default=hex(cust_pico.DefaultFSBaseAddress), 
-                        required=False,   
-                        help="base offset in UF2 for filesystem")
+    parser.add_argument('--pico-flash', required=False, 
+                            type=int,
+                            default=cust_pico.PICO_FLASH_SIZE_BYTES_DEFAULT,
+                            help="Size of flash chip on board (bytes) [%(default)s]")
+    
+    parser.add_argument('--fs-bytes', required=False, 
+                            type=int,
+                            default=cust_pico.MICROPY_HW_FLASH_STORAGE_BYTES_DEFAULT,
+                            help="Size reserved on chip for uPython FS (bytes) [%(default)s]")
     parser.add_argument('--family', default=cust_pico.DefaultFamilyName, 
                         required=False,   
                         help=f"Chip family name [{cust_pico.DefaultFamilyName}]")
@@ -44,20 +50,10 @@ def get_args():
     return parser.parse_args()
 
     
-
-def get_offset(args):
-    v = 0
-    try:
-        v = int(args.fs_offset)
-    except ValueError:
-        v = int(args.fs_offset, 16)
-    
-    return v
-    
 def main():
     args = get_args()
     
-    lfs = cust_pico.prep_filesystem(args.fs_root)
+    lfs = cust_pico.prep_filesystem(args.fs_root, cust_pico.get_blockcount(args))
     tmp = tempfile.NamedTemporaryFile('wb', delete=False)
     cust_pico.write_filesystem(lfs, tmp)
     
@@ -67,7 +63,7 @@ def main():
     if uf2.header.family is None:
         print(f"Invalid family '{args.family}'")
         return
-    cust_pico.append_fs_to(uf2, tmp.name, get_offset(args))
+    cust_pico.append_fs_to(uf2, tmp.name, cust_pico.get_offset(args))
     uf2.to_file(args.out)
     
 if __name__ == '__main__':
